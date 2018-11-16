57d5b0ce22159cdc1594ee9a3f660460d60b19f3
Malo Skrylevo <majioa@yandex.ru>
2018-11-16 16:53:29 +0300

    Улучшение #3615: Для с8, p8, c7, c6 список майнтейнеров пуст
    From now for the branches с8, p8, c7, c6 the title with supported by
    Basealt SPO Team is shewn.

--- app/models/branching_maintainer.rb
+++ app/models/branching_maintainer.rb
@@ -20,10 +20,6 @@ class BranchingMaintainer < ApplicationRecord
 
    def update_count!
       srpms_count = maintainer.acl_names.joins(:branch_path).where(branch_paths: { branch_id: branch }).count
-      if srpms_count == 0
-         # TODO fix it and choose it instead of acls
-         srpms_count = maintainer.gear_names.joins(:branch_paths).where(branch_paths: { branch_id: branch }).count
-      end
 
       self.update!(srpms_count: srpms_count)
    end
