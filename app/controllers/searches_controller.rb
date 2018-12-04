2f9022d08b2db8bec772d3a12c50b50a95e504d6
Malo Skrylevo <majioa@yandex.ru>
2018-12-04 16:06:59 +0300

    Fixed blank search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,37 +1,46 @@
 # frozen_string_literal: true
 
 class SearchesController < ApplicationController
-  before_action :fix_arches
-  before_action :set_branch
+   before_action :fix_arches
+   before_action :set_branch
 
-  def show
-    @branches = Branch.all
-    if params[:query].blank? and params[:arch].blank?
-      redirect_to controller: 'home', action: 'index'
-    else
-      @spkgs = Package::Src.b(@branch.slug)
-                           .a(@arches)
-                           .q(params[:query])
-                           .order("packages.name")
-                           .unscope(:select)
-                           .select("DISTINCT on(qs.rank, packages.name) packages.*, qs.rank")
-                           .page(params[:page])
+   def show
+      @branches = Branch.all
+      if params[:query].blank? and params[:arch].blank?
+         redirect_to controller: 'home', action: 'index'
+      else
+         @spkgs = Package::Src.b(@branch.slug)
+                              .a(@arches)
+                              .q(params[:query])
+                              .includes(:branch)
+                              .order("packages.name")
+                              .unscope(:select)
+                              .select(select_sql)
+                              .page(params[:page])
 
-      if @spkgs.total_count == 1 && @spkgs.first.branches.first
-        redirect_to(srpm_path(@spkgs.first.branches.first, @spkgs.first), status: 302)
+         if @spkgs.total_count == 1 && @spkgs.first.branches.first
+           redirect_to(srpm_path(@spkgs.first.branches.first, @spkgs.first), status: 302)
+         end
       end
-    end
-  end
+   end
 
-  protected
+   protected
 
-  # fixes noarch arch call when blank TODO
-  def fix_arches
-    arches = [ params[:arch] ].flatten.compact.select { |a| a.present? }
-    @arches = arches.any? { |a| a != 'noarch' } && arches || nil
-  end
+   # fixes noarch arch call when blank TODO
+   def fix_arches
+      arches = [ params[:arch] ].flatten.compact.select { |a| a.present? }
+      @arches = arches.any? { |a| a != 'noarch' } && arches || nil
+   end
 
-  def set_branch
-     @branch = Branch.find_or_initialize_by(slug: params[:branch]).decorate
-  end
+   def set_branch
+      @branch = Branch.find_or_initialize_by(slug: params[:branch]).decorate
+   end
+
+   def select_sql
+      if params[:query].blank?
+         select = "DISTINCT on(packages.name) packages.*"
+      else
+         select = "DISTINCT on(qs.rank, packages.name) packages.*, qs.rank"
+      end
+   end
 end
