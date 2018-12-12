377eb8de0c39232fb521239401d6072c144118b3
Malo Skrylevo <majioa@yandex.ru>
2018-12-12 13:54:19 +0300

    Change cache gem to dalli again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,10 +15,10 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if ENV['CACHE']
     config.action_controller.perform_caching = true
 
-    config.cache_store = :mem_cache_store
+    config.cache_store = :dalli_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{ 2.days.seconds.to_i }"
     }
