ff44dd3c994664f1c7788dba445d48b81ed76acd
Malo Skrylevo <majioa@yandex.ru>
2019-04-26 14:58:46 +0300

    Updated PumaWorkerKiller settings
    ! increased RAM limitation for Puma worker killer to 4Gb
    + added debug lines
    + increased run frequency for Puma worker killer to 60 sec

--- config/puma.rb
+++ config/puma.rb
@@ -34,10 +34,12 @@ if Rails.env.production? || Rails.env.staging?
       require 'puma_worker_killer'
 
       PumaWorkerKiller.config do |config|
-         config.ram           = 1024 # mb
-         config.frequency     = 30    # seconds
+         config.ram           = 4096 # mb
+         config.frequency     = 60    # seconds
          config.percent_usage = 0.98
          config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
+         config.reaper_status_logs = true
+         config.pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed" }
       end
       PumaWorkerKiller.start
    end
