387ba59b0cec4e792dafd2bfadecfe285e95c263
Malo Skrylevo <majioa@yandex.ru>
2019-06-04 16:56:09 +0300

    Increase rack timeout
    - increase rack timeout to 240 seconds due to heavy load import

--- config/initializers/rack-timeout.rb
+++ config/initializers/rack-timeout.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 if defined? Rack::Timeout
-   Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 60
+   Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 240, wait_timeout: 240, wait_overtime: 240
 end
