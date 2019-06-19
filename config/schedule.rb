691662fb3ec41c6a521234ca77505ec02ed6227d
Malo Skrylevo <majioa@yandex.ru>
2019-06-19 19:08:57 +0300

    Updated time run schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,42 +1,42 @@
 # frozen_string_literal: true
 
-every 1.day, at: '3:00' do
-   rake 'update:branches update:lost[true]'
-end
-
-every 1.hour do
-   rake 'task:update'
-end
-
-every 3.hours, at: '0:35' do
+every 3.hours, at: '2:35 am' do
    rake 'repo:gears repo:srpms repo:people'
 end
 
-every 3.hours, at: '2:05' do
+every 3.hours, at: '2:05 am' do
    rake 'bugs:update'
 end
 
-every 3.hours, at: '2:15' do
+every 3.hours, at: '2:15 am' do
    rake 'ftbfs:update'
 end
 
-every 3.hours, at: '2:25' do
+every 3.hours, at: '2:25 am' do
    rake 'novelties:update'
 end
 
-every 1.day, at: '3:25' do # takes about 20 min
+every 1.day, at: '3:00 am' do
+   rake 'update:branches update:lost[true]'
+end
+
+every 1.day, at: '1:00 am' do
+   rake 'task:update'
+end
+
+every 1.day, at: '3:25 am' do # takes about 20 min
    rake 'repocop:update'
 end
 
-every 1.day, at: '3:45' do # takes about 5 min
+every 1.day, at: '3:45 am' do # takes about 5 min
    rake 'fix:branching_maintainers'
 end
 
-every :sunday, at: '03:30' do
+every :sunday, at: '03:30 am' do
   rake 'sitemap:clean sitemap:refresh'
 end
 
-every :sunday, at: '06:30' do
+every :sunday, at: '06:30 am' do
 #  rake 'perlwatch:update'
 end
 
