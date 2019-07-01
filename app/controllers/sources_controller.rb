bb4396ce47e4822ca12fe81d111cc6c7ed1c266a
Malo Skrylevo <majioa@yandex.ru>
2019-07-01 18:53:04 +0300

    Fix sources
    - fix source controller to support download method, and filter out the
      non-unique sources for the non-hidden packages
    - fix sources view

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class SourcesController < ApplicationController
-   before_action :set_evrb
-   before_action :fetch_spkg
+   before_action :set_evrb, except: :download
+   before_action :fetch_spkg, except: %i(download)
    before_action :fetch_spkgs_by_name, only: %i(index)
+   before_action :fetch_source, only: %i(download)
    before_action :fetch_bugs, only: :index
 
    def index
-      @sources = Source.where(package: @spkgs).presented.uniq_by(:content)
+      @sources = Source.where(package: @spkgs).uniq_by(:content)
+   end
+
+   def download
+      send_data(@source.content, disposition: 'attachment', filename: @source.filename)
    end
 
    protected
@@ -31,8 +36,30 @@ class SourcesController < ApplicationController
       @evrb = params[:evrb]
    end
 
+   def package_attrs
+      attrs = {
+         name: params[:reponame],
+         arch: "src"
+      }
+
+      if /(?:(?<epoch>\d+):)?(?<version>[^-]+)-(?<release>[^-]+)-(?<built_at>\d+)$/ =~ params[:evrb]
+         attrs.merge!(epoch: epoch,
+                      version: version,
+                      release: release,
+                      buildtime: Time.at(built_at.to_i))
+      end
+
+      attrs
+   end
+
    def fetch_bugs
       @all_bugs = AllBugsForSrpm.new(spkg: @spkg, branch: @branch).decorate
       @opened_bugs = OpenedBugsForSrpm.new(spkg: @spkg, branch: @branch).decorate
+   end
+
+   def fetch_source
+      attrs = { filename: params[:source_name], packages: package_attrs }
+      sources = Source.joins(:package).where(attrs)
+      @source = sources.find { |source| source.content? } || raise(ActiveRecord::RecordNotFound)
    end
 end
