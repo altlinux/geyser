6aa036378f2570927fe717cf4a26518842d4c2c1
Malo Skrylevo <majioa@yandex.ru>
2020-05-20 23:39:21 +0300

    Errored staging server address
    * staging server address to predvridlo.office.basealt.ru in deploy and
      environment.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -9,12 +9,12 @@ set :rails_env, 'staging'
 
 set :branch, ENV['BRANCH'] || "staging"
 
-server '10.10.3.49', user: 'apache', roles: ['app', 'db', 'web']
+server 'predvridlo.office.basealt.ru', user: 'apache', roles: ['app', 'db', 'web']
 
 set :ssh_options, port: 22
 
 set :rvm_type, :user
 
-set :app_server_host, "staging.office.basealt.ru"
+set :app_server_host, "predvridlo.office.basealt.ru"
 
-set :nginx_domains, "staging.office.basealt.ru"
+set :nginx_domains, "predvridlo.office.basealt.ru"
