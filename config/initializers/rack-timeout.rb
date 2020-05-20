9e98d07cc97f8b8f5c68b74cc91c29134cf0707a
Malo Skrylevo <majioa@yandex.ru>
2020-05-20 22:56:02 +0300

    Disable rack-timeout for development
    * disabling rack-timeout for development

--- config/initializers/rack-timeout.rb
+++ config/initializers/rack-timeout.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
-require 'rack-timeout'
-Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 240, wait_timeout: 240, wait_overtime: 240
+if !Rails.env.development?
+   require 'rack-timeout'
+   Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 240, wait_timeout: 240, wait_overtime: 240
+end
