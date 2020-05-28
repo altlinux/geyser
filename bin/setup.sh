69c18ef7b54718c32e96310bc1d9803c42f79553
Malo Skrylevo <majioa@yandex.ru>
2020-05-28 17:13:56 +0300

    Updated setup.sh
    + archive folder
    ! creating folders
    ! setup.sh permissions

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo mkdir /beehive/ /archive_git/ /people/ /ports/ /roland/ /tasks
-sudo chmod 777 /archive_git/ /people/ /beehive/ /ports/ /roland/ /tasks
+sudo mkdir -p /beehive/ /archive_git/ /people/ /ports/ /roland/ /tasks /gears /home/nosrpm/e2k/ /srpms /archive
+sudo chmod 777 /archive_git/ /people/ /beehive/ /ports/ /roland/ /tasks /gears /home/nosrpm/e2k/ /srpms /archive
 
 sshfs -p 222 apache@packages.altlinux.org:/srpms/ /srpms
 sshfs -p 222 apache@packages.altlinux.org:/gears/ /gears
@@ -11,5 +11,6 @@ sshfs -p 222 apache@packages.altlinux.org:/beehive/ /beehive/
 sshfs -p 222 apache@packages.altlinux.org:/roland/ /roland/
 sshfs -p 222 apache@packages.altlinux.org:/ports/ /ports/
 sshfs -p 222 apache@packages.altlinux.org:/tasks/ /tasks
+sshfs -p 222 apache@packages.altlinux.org:/archive/ /archive
 
 sshfs -p 222 apache@packages.altlinux.org:/home/nosrpm/e2k/ /home/nosrpm/e2k/
