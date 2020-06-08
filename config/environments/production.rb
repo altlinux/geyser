583eca1df5c7e92cbd4f35f77a545b7dfc432823
Malo Skrylevo <majioa@yandex.ru>
2020-06-08 17:53:32 +0300

    update dalli to p.a.o

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,8 +57,8 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store
-
+  config.cache_store = :dalli_store, 'packages.altlinux.org'
+  #
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "geyser_#{Rails.env}"
