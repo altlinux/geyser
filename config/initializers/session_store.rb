53ea2e18e9a4eebf22cfbdd3d572fe58a7f59bf4
Malo Skrylevo <majioa@yandex.ru>
2020-06-17 12:46:28 +0300

    fix cache server
    * cache access to p.a.o instead of localhost

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,5 @@
 
 # Be sure to restart your server when you modify this file.
 # TODO: remove this
-
-Rails.application.config.session_store :cookie_store, key: '_geyser_session'
+require 'action_dispatch/middleware/session/dalli_store'
+Rails.application.config.session_store :dalli_store, :memcache_server => ['packages.altlinux.org'], :namespace => 'sessions', :key => '_geyser_session'
