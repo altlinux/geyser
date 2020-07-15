ef49073f6c7e03aed544aef8c375f5e4577bca42
Malo Skrylevo <majioa@yandex.ru>
2020-07-15 19:44:39 +0300

    Disconditionals in setup.sh
    ! mount options and folders in setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo mkdir -p /beehive/ /archive_git/ /people/ /ports/ /roland/ /tasks /gears /home/nosrpm/e2k/ /srpms /archive
-sudo chmod 777 /archive_git/ /people/ /beehive/ /ports/ /roland/ /tasks /gears /home/nosrpm/e2k/ /srpms /archive
+sudo mkdir -p /beehive/ /people/ /ports/ /roland/ /tasks /gears /home/nosrpm/e2k/ /srpms /archive
+sudo chmod 777 /people/ /beehive/ /ports/ /roland/ /tasks /gears /home/nosrpm/e2k/ /srpms /archive
 
-sshfs -p 222 apache@packages.altlinux.org:/srpms/ /srpms
-sshfs -p 222 apache@packages.altlinux.org:/gears/ /gears
-sshfs -p 222 apache@packages.altlinux.org:/people/ /people/
+sshfs -p 222 apache@packages.altlinux.org:/srpms/ /srpms -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
+sshfs -p 222 apache@packages.altlinux.org:/gears/ /gears -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
+sshfs -p 222 apache@packages.altlinux.org:/people/ /people/ -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
 
-sshfs -p 222 apache@packages.altlinux.org:/beehive/ /beehive/
-sshfs -p 222 apache@packages.altlinux.org:/roland/ /roland/
-sshfs -p 222 apache@packages.altlinux.org:/ports/ /ports/
-sshfs -p 222 apache@packages.altlinux.org:/tasks/ /tasks
-sshfs -p 222 apache@packages.altlinux.org:/archive/ /archive
+sshfs -p 222 apache@packages.altlinux.org:/beehive/ /beehive/ -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
+sshfs -p 222 apache@packages.altlinux.org:/roland/ /roland/ -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
+sshfs -p 222 apache@packages.altlinux.org:/ports/ /ports/ -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
+sshfs -p 222 apache@packages.altlinux.org:/tasks/ /tasks -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
+sshfs -p 222 apache@packages.altlinux.org:/archive/ /archive -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
 
-sshfs -p 222 apache@packages.altlinux.org:/home/nosrpm/e2k/ /home/nosrpm/e2k/
+sshfs -p 222 apache@packages.altlinux.org:/home/nosrpm/e2k/ /home/nosrpm/e2k/ -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
