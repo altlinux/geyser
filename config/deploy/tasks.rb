d0b516861fb148295439b3414dc385502b52e53c
Malo Skrylevo <majioa@yandex.ru>
2020-07-24 14:27:21 +0300

    Fixed domain for tasks deploy
    * allowing to redefine deploy host with HOST env

--- config/deploy/tasks.rb
+++ config/deploy/tasks.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
+host = ENV['HOST'] || 'dnisce.office.basealt.ru'
+
 set :repo_url, 'git@github.com:altlinux/geyser.git'
 
 set :deploy_to, '/var/www/geyser'
 
 set :rails_env, 'production'
 
-server 'dubrovice.office.basealt.ru', user: 'apache', roles: %w(rake)
+server host, user: 'apache', roles: %w(rake)
 
 set :ssh_options, port: 22
 
 set :rvm_type, :user
 # set :rvm_custom_path, '~/.rvm'          # only needed if not detected
 
-set :app_server_host, "dubrovice.office.basealt.ru"
-
+set :app_server_host, host
